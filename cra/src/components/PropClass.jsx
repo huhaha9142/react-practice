d96bdecca6c1bee0dd4aadcbc9e47881f10c7247
박상혁 <huhaha9142@naver.com>
2021-09-23 14:16:51 +0900

    Props in Function component

--- cra/src/components/PropClass.jsx
+++ cra/src/components/PropClass.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import PropFunc from './components/PropFunc';
 
 class PropClass extends Component {
     constructor(props) {
